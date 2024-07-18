a6d29283b4c41d7a5f84925b768524db5cc4a2c9
csizma <csizmadia.kata@gmail.com>
2024-07-18 14:04:47 +0200

    add complexity

--- vector.c
+++ vector.c
@@ -11,16 +11,19 @@ void delete_from_index_and_shift_vector(vector* v, int index);
 void print_indexing_error(size_t cur_capacity, size_t cur_num_elems, size_t ix);
 void print_overindexing_error(void);
 
+//Memory O N where N is the array size. Note, that if N is really small, the pointer sizes and longs will take the majority of space, while when N gets big, 
+//they get less significant.
+//Runtime: O 1 (mallocs should be the slowest part)
 vector* mk_vector(int* p, size_t size)
 {
-  vector* pointer = malloc(sizeof(vector));
-  if(pointer == NULL)
+  vector* vector = malloc(sizeof(vector));
+  if(vector == NULL)
   {
     printf("Pointer memory allocation failed, the program terminates.\n");
     return NULL;
   }
-  pointer->capacity = size;
-  pointer->num_elems = size;
+  vector->capacity = size;
+  vector->num_elems = size;
   int* array = malloc(size * sizeof(int));
   if(array == NULL)
   {
@@ -28,10 +31,12 @@ vector* mk_vector(int* p, size_t size)
     return NULL;
   }
   memcpy(array, p, size * sizeof(int));
-  pointer->elems = array;
-  return pointer;
+  vector->elems = array;
+  return vector;
 }
 
+//Memory O1
+//Runtime: freeing runtime unkown, probably O1
 void free_vector(vector* v)
 {
   free(v->elems);
@@ -39,6 +44,9 @@ void free_vector(vector* v)
   return;
 }
 
+//Memory ON where N is the array size. Note, that if N is really small, the pointer sizes and longs will take the majority of space, 
+//while when N gets big, they get less significant.
+//Runtime: O N where N is the elem num (Note malloc time is unknown, with small elem num it could be the slowest part)
 void resize(vector* v, size_t new_capacity)
 {
   size_t cur_capacity = v->capacity;
@@ -61,6 +69,9 @@ void resize(vector* v, size_t new_capacity)
   return;
 }
 
+//Memory ON where N is the array size. Note, that if N is really small, the pointer sizes and longs will take the majority of space, 
+//while when N gets big, they get less significant.
+//Runtime: O1
 int get(vector* v, size_t ix)
 {
   size_t cur_num_elems = v->num_elems;
@@ -76,34 +87,47 @@ int get(vector* v, size_t ix)
   }
 }
 
+//Memory ON where N is the array size. Note, that if N is really small, the pointer sizes and longs will take the majority of space, 
+//while when N gets big, they get less significant.
+//Runtime: ON where N is the element num that has to be shifted + inserted.
 void insert_to_index_and_shift_vector(vector* v, int x, int index)
 {
   size_t cur_num_elems = v->num_elems;
-  size_t elems_after_index = cur_num_elems - index;
-    memmove(&v->elems[index + 1], &v->elems[index], elems_after_index * sizeof(int));
+  size_t elem_num_after_index = cur_num_elems - index;
+    memmove(&v->elems[index + 1], &v->elems[index], elem_num_after_index * sizeof(int));
     v->elems[index] = x;
     v->num_elems++;
 }
 
+//Memory ON where N is the array size. Note, that if N is really small, the pointer sizes and longs will take the majority of space, 
+//while when N gets big, they get less significant.
+//Runtime: ON where N is the element num that has to be shifted + deleted.
 void delete_from_index_and_shift_vector(vector* v, int index)
 {
   size_t cur_num_elems = v->num_elems;
-  size_t elems_after_index = cur_num_elems - index;
-    memmove(&v->elems[index], &v->elems[index + 1], elems_after_index * sizeof(int));
+  size_t elem_num_after_index = cur_num_elems - index;
+    memmove(&v->elems[index], &v->elems[index + 1], elem_num_after_index * sizeof(int));
     v->num_elems--;
 }
 
+//Memory O1
+//Runtime: O1
 void print_capacity_error(size_t cur_capacity, size_t cur_num_elems)
 {
   printf("Sth went wrong the cur_capacity is %li, and the cur_num_elems is %li, the function terminates.\n", cur_capacity, cur_num_elems);
 }
 
+//Memory O1
+//Runtime: O1
 void print_indexing_error(size_t cur_capacity, size_t cur_num_elems, size_t ix)
 {
   printf("Sth went wrong the cur_capacity is %li, and the cur_num_elems is %li, and the requested index is %li the function terminates.\n",
      cur_capacity, cur_num_elems, ix);
 }
 
+//Memory ON where N is the array size. Note, that if N is really small, the pointer sizes and longs will take the majority of space, 
+//while when N gets big, they get less significant.
+//Runtime: ON where N is the element num (because of possible resize)
 void push_back(vector* v, int x)
 {
   size_t cur_capacity = v->capacity;
@@ -125,6 +149,9 @@ void push_back(vector* v, int x)
   return;
 }
 
+//Memory ON where N is the array size. Note, that if N is really small, the pointer sizes and longs will take the majority of space, 
+//while when N gets big, they get less significant.
+//Runtime: ON where N is the element num.
 void push_front(vector* v, int x)
 {
   size_t cur_capacity = v->capacity;
@@ -146,6 +173,9 @@ void push_front(vector* v, int x)
   return;
 }
 
+//Memory ON where N is the array size. Note, that if N is really small, the pointer sizes and longs will take the majority of space, 
+//while when N gets big, they get less significant.
+//Runtime: ON where N is the element num due to possible resize.
 void insert_at(vector* v, size_t ix, int elem)
 {
   size_t cur_num_elems = v->num_elems;
@@ -167,7 +197,9 @@ void insert_at(vector* v, size_t ix, int elem)
   return;
 }
 
-
+//Memory ON where N is the array size. Note, that if N is really small, the pointer sizes and longs will take the majority of space, 
+//while when N gets big, they get less significant.
+//Runtime: ON where N is the element num that has to be shifted + deleted.
 void remove_at(vector* v, size_t ix)
 {
   size_t cur_num_elems = v->num_elems;
@@ -183,6 +215,8 @@ void remove_at(vector* v, size_t ix)
   return;
 }
 
+//Memory ON where N is the vector size.
+//Runtime: O1 malloc is the slowest.
 vector_iterator begin(const vector* v)
 {
   vector_iterator* iterator = malloc(sizeof(vector_iterator));
@@ -196,11 +230,15 @@ vector_iterator begin(const vector* v)
   return *iterator;
 }
 
+//Memory O1
+//Runtime: O1
 void print_overindexing_error(void)
 {
   printf("Indexing beyond the last element. The function terminates.\n");
 }
 
+//Memory O1
+//Runtime: O1
 void next(vector_iterator* it)
 {
   if(!finished(*it))
@@ -214,6 +252,8 @@ void next(vector_iterator* it)
   }
 }
 
+//Memory O1
+//Runtime: O1
 bool finished(vector_iterator it)
 {
   size_t cur_num_elems = it.vec->num_elems;
@@ -221,6 +261,8 @@ bool finished(vector_iterator it)
   return cur_ix >= cur_num_elems;
 }
 
+//Memory O1
+//Runtime: O1
 int get_elem(vector_iterator it)
 {
   if(!finished(it))
@@ -234,6 +276,10 @@ int get_elem(vector_iterator it)
   }
 }
 
+
+//Memory ON N is the array size. Note, that if N is really small, the pointer sizes and longs will take the majority of space, 
+//while when N gets big, they get less significant.
+//Runtime: ON where N is the element num
 long sum(vector_iterator* it)
 {
   long res = 0;
@@ -245,6 +291,8 @@ long sum(vector_iterator* it)
   return res;
 }
 
+//Memory ON N is the array size.
+//Runtime: ON where N is the sum of the element nums
 bool equals(const vector* lhs, const vector* rhs)
 {
   if (lhs->num_elems == rhs->num_elems)
